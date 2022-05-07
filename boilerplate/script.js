ced6367106e1128b81cbbd8d1f6e617ed82782b7
unknown <arestre@ncsu.edu>
2022-05-07 10:53:12 -0400

    adding modal

--- boilerplate/script.js
+++ boilerplate/script.js
@@ -1,5 +1,73 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("apitestbtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  var str = "clicked";
+  console.log(str);
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
 const url = "http://www.omdbapi.com/?apikey=39fdf732&"
 
+// var searchButton = document.getElementById("apitestbtn");
 
+// // searchButton.onclick = function(){
+//     getData()
+// };
+
+async function getData() {
+    var title = $("#title").val();
+    var year = $("#year").val(); 
+    var queryString = "http://www.omdbapi.com/?apikey=39fdf732&t=" + title + "&y=" + year + "&plot=short&r=json";
+
+}
 
 //api key 39fdf732
+
+// async function getData() {
+//     var locationInput = document.getElementById("modalLocation");
+//  var zipcode = locationInput.value.trim();
+  
+//     var raw = JSON.stringify({
+//           "data": {
+//             "filterRadius": {
+//               "miles": 50,
+//               "postalcode": zipcode
+//             }
+//         }});
+//     const response = await fetch(url, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/vnd.api+json',
+//            'Authorization': 'u2301vV9'
+//         },
+//        body: raw
+//    });
+//    const data = await response.json();
+
