a371f31cc3e58e4ac9c14e64b3049d2d77b8f108
Sarah Thompson <sthompson@fintechprocessing.com>
2022-05-15 08:45:22 -0400

    added auth.js file for user auth routes

--- server.js
+++ server.js
@@ -20,6 +20,20 @@ const sess = {
   })
 };
 
+***
+app.use(express.json())
+
+const users = []
+
+app.get('/users', (req, res)=> {
+  res.json(users)
+})
+app.post('/users', (req, res)=> {
+  const user = { name: req.body.name, password:req.body.password }
+  users.push(user)
+  res.status(201).send()
+})***
+
 app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
