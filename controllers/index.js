1fc927a7d43c0c6983c866f6c5a6e65ef29331d5
Sarah Thompson <sthompson@fintechprocessing.com>
2022-06-07 09:56:57 -0400

    trying to reroute the controllers

--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,8 @@ const homeRoutes = require('./home-routes.js');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+); 
+
 module.exports = router;
